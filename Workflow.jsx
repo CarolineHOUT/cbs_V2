b525bbefa2ea8aee8bb905fb3d4dd531c8fab535
CarolineHOUT <Houttevillecaroline38@gmail.com>
2026-03-15 10:17:47 +0100

    Create Workflow.jsx

---
+++ Workflow.jsx
@@ -0,0 +1,16 @@
+export default function Workflow(){
+
+  return(
+
+    <div>
+
+      <h3>Workflow actions</h3>
+
+      <div>Action 1 : Demande SSIAD</div>
+      <div>Action 2 : Demande DAC</div>
+
+    </div>
+
+  )
+
+}
