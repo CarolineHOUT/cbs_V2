42a87eea8fb03888a3fcee0e1b670e5e4167d027
CarolineHOUT <Houttevillecaroline38@gmail.com>
2026-03-15 10:18:27 +0100

    Create Note.jsx

---
+++ Note.jsx
@@ -0,0 +1,19 @@
+export default function Notes(){
+
+  return(
+
+    <div>
+
+      <h3>Communication équipe</h3>
+
+      <textarea
+        placeholder="Ajouter une note"
+        rows="3"
+        cols="40"
+      />
+
+    </div>
+
+  )
+
+}
