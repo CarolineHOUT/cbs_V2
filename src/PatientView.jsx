fae91cd870a58c1db9d678b242faedcb62f793c1
CarolineHOUT <Houttevillecaroline38@gmail.com>
2026-03-15 11:02:43 +0100

    fix PatientView file path

--- src/PatientView.jsx
+++ src/PatientView.jsx
@@ -3,31 +3,23 @@ import Workflow from "./Workflow.jsx";
 import IntakeForm from "./IntakeForm.jsx";
 
 export default function PatientView({ patient, onBack }) {
+return (
+<div>
+<button onClick={onBack}>Retour</button>
 
-  return (
+<h2>
+{patient.prenom} {patient.nom}
+</h2>
 
-    <div>
+<div>INS : {patient.ins}</div>
+<div>Service : {patient.service}</div>
+<div>Blocage : {patient.blocage}</div>
 
-      <button onClick={onBack}>
-        Retour
-      </button>
+<hr />
 
-      <h2>
-        {patient.prenom} {patient.nom}
-      </h2>
-
-      <div>INS : {patient.ins}</div>
-      <div>Service : {patient.service}</div>
-      <div>Blocage : {patient.blocage}</div>
-
-      <hr/>
-
-      <IntakeForm />
-
-      <Workflow />
-
-      <Notes />
-
-    </div>
-  );
+<IntakeForm />
+<Workflow />
+<Notes />
+</div>
+);
 }
