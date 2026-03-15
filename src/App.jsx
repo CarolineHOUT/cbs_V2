a9cadd9f5d877f67f7aa6acb5e5f5b1d70819646
CarolineHOUT <Houttevillecaroline38@gmail.com>
2026-03-15 11:05:18 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,8 @@
-import { useState } from "react";
-import Dashboard from "./Dashboard.jsx";
-import PatientView from "./PatientView.jsx";
-
-const patientsData = [
-  {
-    id: 1,
-    nom: "Renard",
-    prenom: "Camille",
-    ins: "1800612345987",
-    service: "Oncologie",
-    blocage: "Coordination ville insuffisante",
-    score: 6
-  },
-  {
-    id: 2,
-    nom: "Martin",
-    prenom: "Pierre",
-    ins: "1800654321789",
-    service: "Médecine",
-    blocage: "Suivi simple",
-    score: 3
-  }
-];
-
 export default function App() {
-
-  const [patients] = useState(patientsData);
-  const [selectedPatient, setSelectedPatient] = useState(null);
-
-  return (
-    <div style={{ padding: 30 }}>
-
-      <h1>CARABBAS</h1>
-
-      {!selectedPatient && (
-        <Dashboard
-          patients={patients}
-          onOpenPatient={setSelectedPatient}
-        />
-      )}
-
-      {selectedPatient && (
-        <PatientView
-          patient={selectedPatient}
-          onBack={() => setSelectedPatient(null)}
-        />
-      )}
-
-    </div>
-  );
+return (
+<div style={{ padding: 30 }}>
+<h1>CARABBAS</h1>
+<p>Le projet fonctionne.</p>
+</div>
+);
 }
