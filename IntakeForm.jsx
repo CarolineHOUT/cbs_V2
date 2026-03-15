78a30b591ad4e9063d28d44b3f98e5ec01886019
CarolineHOUT <Houttevillecaroline38@gmail.com>
2026-03-15 10:17:05 +0100

    Create IntakeForm.jsx

---
+++ IntakeForm.jsx
@@ -0,0 +1,20 @@
+export default function IntakeForm(){
+
+  return(
+
+    <div>
+
+      <h3>Recueil d'entrée</h3>
+
+      <input placeholder="Lieu de vie"/>
+      <br/><br/>
+
+      <input placeholder="Aidant principal"/>
+      <br/><br/>
+
+      <input placeholder="Freins identifiés"/>
+
+    </div>
+
+  )
+}
