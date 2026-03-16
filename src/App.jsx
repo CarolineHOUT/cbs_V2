e0a57d6758160a5cc040f69131b90c88610db461
CarolineHOUT <Houttevillecaroline38@gmail.com>
2026-03-16 22:29:00 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
-import PatientsView from "./PatientsView";
 import PatientView from "./PatientView";
 
 export default function App() {
@@ -10,7 +9,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/patients" element={<PatientsView />} />
         <Route path="/patient/:id" element={<PatientView />} />
       </Routes>
     </BrowserRouter>
