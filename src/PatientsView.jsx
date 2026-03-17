76a983b672f4513da048e7d876e68ebbb208b318
CarolineHOUT <Houttevillecaroline38@gmail.com>
2026-03-17 06:22:47 +0100

    Create PatientsView.jsx

---
+++ src/PatientsView.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./PatientsView.css";
+
+export default function PatientsView() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Vue Patients</h1>
+      <p>La page Patients fonctionne.</p>
+    </div>
+  );
+}
